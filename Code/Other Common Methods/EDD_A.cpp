67c8c4675193e4e2f1449a0e64c123e967ec3868
Ravi Shankar <ravishankar98910@gmail.com>
2020-10-30 03:20:47 +0530

    EDD-A Algorithm

--- Code/Other Common Methods/EDD_A.cpp
+++ Code/Other Common Methods/EDD_A.cpp
@@ -5,6 +5,21 @@ using namespace std;
 #define F first
 #define ll long long int
 
+void printTree(vector<vector<ll>> dp){
+	ll size = dp.size();
+	for(ll i=0;i<size;i++){
+		ll temp = dp[i].size();
+		if(temp > 0){
+			cout<<i<<": ";
+			for(ll j=0;j<temp;j++){
+				cout<<dp[i][j]<<" ";
+			}
+			cout<<endl;
+		}
+	}
+	return;
+}
+
 map<pair<ll, ll>, pair<ll, ll>> closestNodes(vector<vector<ll>> &G, vector<bool> &visited){
 	map<pair<ll, ll>, pair<ll,ll>> mp;
 	ll size = visited.size();
@@ -23,6 +38,54 @@ map<pair<ll, ll>, pair<ll, ll>> closestNodes(vector<vector<ll>> &G, vector<bool>
 	}
 
 	return mp;
+}
+
+void BFS(vector<vector<ll>> &cmst, vector<ll> &depths, ll node, ll height){
+	deque<pair<ll, ll>> dq;
+	dq.push_back({node, height});
+	depths[node] = height;
+	while(!dq.empty()){
+		pair<ll, ll> p = dq.front();
+		dq.pop_front();
+		ll index = p.F;
+		ll height = p.S;
+		ll size = cmst[index].size();
+		for(ll i=0;i<size;i++){
+			ll child = cmst[index][i];
+			if(depths[child] > height + 1){
+				depths[child] = height + 1;
+				dq.push_back({child, height+1});
+			}
+		}
+	}
+	return;
+}
+
+void DFS(vector<vector<ll>> &cmst, vector<vector<ll>> &eddA, vector<ll> &depths, map<ll, ll> &destination_edge, vector<bool> &visited, ll index, ll d_limit){
+	ll size = cmst[index].size();
+	for(ll i=0;i<size;i++){
+		ll child = cmst[index][i];
+		if(!visited[child] && (depths[child] == depths[index]+1)){
+			if(depths[child] <= d_limit && destination_edge.find(child) != destination_edge.end()){
+				eddA[index].push_back(child);
+				visited[child] = true;
+				DFS(cmst, eddA, depths, destination_edge, visited, child, d_limit);
+			}
+			else if(destination_edge.find(child) == destination_edge.end()){
+				eddA[index].push_back(child);
+				visited[child] = true;
+				DFS(cmst, eddA, depths, destination_edge, visited, child, d_limit);
+			}
+			else if(depths[child] > d_limit && destination_edge.find(child) != destination_edge.end()){
+				depths[child] = 1;
+				eddA[0].push_back(child);
+				visited[child] = true;
+				BFS(cmst, depths, child, 1);
+				DFS(cmst, eddA, depths, destination_edge, visited, child, d_limit);
+			} 
+		}
+	}
+	return;
 }
 
 vector<vector<ll>> CMST(vector<vector<ll>> &G, map<ll, ll> &destination_edge, ll V){
@@ -65,8 +128,14 @@ vector<vector<ll>> CMST(vector<vector<ll>> &G, map<ll, ll> &destination_edge, ll
 	return cmst;
 }
 
-void EDD_A(){
-	return;
+vector<vector<ll>> EDD_A(vector<vector<ll>> &cmst, map<ll, ll> &destination_edge, ll V, ll d_limit){
+	vector<vector<ll>> eddA(V+1);
+	vector<ll> depths(V+1, INT_MAX);
+	BFS(cmst, depths, 0, 0);
+	vector<bool> visited(V+1, false);
+	visited[0] = true;
+	DFS(cmst, eddA, depths, destination_edge, visited, 0, d_limit);
+	return eddA;
 }
 
 int main(){
@@ -75,6 +144,8 @@ int main(){
 
 	ll d_limit, gamma;   // EDD time constraint limit - d_limit and ratio of cost of C2E and 1-hop E2E transmission - gamma. 
 	cin>>d_limit>>gamma;
+
+	d_limit += 1;
 
 	vector<vector<ll>> G(V+1, vector<ll>());      // Graph G(V, E).
 	map<ll, ll> destination_edge;
@@ -98,16 +169,26 @@ int main(){
 
 	vector<vector<ll>> cmst = CMST(G, destination_edge, V);
 	cout<<"Minimum Steiner Tree Edge List:"<<endl; 
+	printTree(cmst);
+
+	vector<vector<ll>> eddA = EDD_A(cmst, destination_edge, V, d_limit);
+	cout<<endl<<"Final EDD Approximated Tree is:"<<endl;
+	printTree(eddA);
+
+	ll C2E = 0;
+	ll E2E = 0;
 	for(ll i=0;i<=V;i++){
-		ll temp = cmst[i].size();
-		if(temp > 0){
-			cout<<i<<": ";
-			for(ll j=0;j<temp;j++){
-				cout<<cmst[i][j]<<" ";
-			}
-			cout<<endl;
+		if(i==0){
+			C2E += eddA[i].size();
+		}
+		else{
+			E2E += eddA[i].size();
 		}
 	}
-	
+
+	cout<<endl<<"Cost C2E: "<<C2E<<" times of 1-Hop E2E"<<endl;
+	cout<<"Cost E2E: "<<E2E<<" times of 1-Hop E2E"<<endl;
+	cout<<"Total Cost By EDD-A Algorithm: "<<((gamma*C2E)+E2E)<<" times of 1-Hop E2E"<<endl; 
+
 	return 0;
 }
