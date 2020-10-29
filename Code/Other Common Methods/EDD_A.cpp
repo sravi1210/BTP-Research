0256244f5b7ffe61bbbdd080b32a4b4be97c9aab
Ravi Shankar <ravishankar98910@gmail.com>
2020-10-29 13:01:42 +0530

    Connectivity oriented MST

---
+++ Code/Other Common Methods/EDD_A.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+void CMST(){
+	return;
+}
+
+void EDDA(){
+	return;
+}
+
+int main(){
+	ll V, E, R;          // V - Vertex, E - Edges,  R - Destination Edge Servers.
+	cin>>V>>E>>R;
+
+	ll d_limit, gamma;   // EDD time constraint limit - d_limit and ratio of cost of C2E and 1-hop E2E transmission - gamma. 
+	cin>>d_limit>>gamma;
+
+	vector<vector<ll>> G(V+1, vector<ll>());      // Graph G(V, E).
+	map<ll, ll> destination_edge;
+
+	for(ll i=0;i<E;i++){        // Create the graph with given edges.
+		ll node1, node2;
+		cin>>node1>>node2;
+		G[node1].push_back(node2);
+		G[node2].push_back(node1);
+	}
+
+	/*
+		for(ll i=1;i<=V;i++){      // Add edges from cloud 'c' to each edge server 'v'.
+			G[0].push_back(i);
+			G[i].push_back(0);
+		}
+	*/
+
+	for(ll i=0;i<R;i++){    // Create a map of destination edge servers.
+		ll node;
+		cin>>node;
+		destination_edge[node] = 1;
+	}
+
+	return 0;
+}
