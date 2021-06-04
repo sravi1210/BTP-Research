596913d4096319a4136c881fc2ff08d625ff90e6
Ravi Shankar <ravishankar98910@gmail.com>
2021-06-04 23:23:18 +0530

    GNUPLots Updated

--- Code Sem 8/GNUPlot Graphs/N3.sh
+++ Code Sem 8/GNUPlot Graphs/N3.sh
@@ -6,4 +6,4 @@ set output 'Set1_LC_VS_Cost.eps'
 set key inside bottom left
 set xlabel 'EDD length constraint L_{limit}'
 set ylabel 'Total EDD cost'
-plot  "N3.dat" using 1:2 title 'Integer Programming' with lines linecolor rgb '#8CB500' linewidth 1.5,"N3.dat" using 1:3 title 'EDD-NSTE' with linespoints linecolor rgb '#9966CC' linewidth 1.5,"N3.dat" using 1:4 title 'EDD-A' with linespoints linecolor rgb '#CC1942' linewidth 1.5,"N3.dat" using 1:5 title 'Greedy' with linespoints linecolor rgb '#002E63' linewidth 1.5,"N3.dat" using 1:6 title 'Random' with linespoints linecolor rgb '#184343' linewidth 1.5
+plot  "N3.dat" using 1:2 title 'Integer Programming' with lines linecolor rgb '#8CB500' linewidth 3,"N3.dat" using 1:3 title 'EDD-NSTE' with linespoints linecolor rgb '#9966CC' linewidth 3,"N3.dat" using 1:4 title 'EDD-A' with linespoints linecolor rgb '#CC1942' linewidth 3,"N3.dat" using 1:5 title 'Greedy' with linespoints linecolor rgb '#002E63' linewidth 3,"N3.dat" using 1:6 title 'Random' with linespoints linecolor rgb '#184343' linewidth 3
